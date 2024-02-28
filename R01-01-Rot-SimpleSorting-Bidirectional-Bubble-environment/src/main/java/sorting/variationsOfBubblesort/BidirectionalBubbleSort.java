5e5c1cc35207a8d0ab6bd91c7b0b7de54775c7ac
Pedro Nascimento da Silva <101910729+Pedronrb@users.noreply.github.com>
2024-02-28 10:09:33 -0300

    Update BidirectionalBubbleSort.java

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
@@ -1,6 +1,7 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * This bubble sort variation has two internal iterations. In the first, it
@@ -9,12 +10,41 @@ import sorting.AbstractSorting;
  * pushing small elements to the left. This process is repeated until the array
  * is sorted.
  */
-public class BidirectionalBubbleSort<T extends Comparable<T>> extends
-		AbstractSorting<T> {
+public class BidirectionalBubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if(verification(array, leftIndex, rightIndex)){
+			this.BidirectionalBubbleSort(array, leftIndex, rightIndex);
+		}
+	}
+
+	private void BidirectionalBubbleSort(T[] array, int leftIndex, int rightIndex) {
+		boolean swapped = true;
+
+		while(swapped){
+			swapped = false;
+
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if(array[i].compareTo(array[i+1]) > 0){
+					Util.swap(array, i, i + 1);
+					swapped = true;
+				}
+			}
+			for(int j = rightIndex; j > leftIndex; j--){
+				if(array[j].compareTo(array[j-1]) < 0){
+					Util.swap(array, j, j-1);
+					swapped = true;
+				}
+			}
+		}
+	}
+
+	private boolean verification(T[] array, int leftIndex, int rightIndex) {
+		boolean status = true;
+		if((leftIndex < 0 || leftIndex >= array.length) || (rightIndex < 0 || rightIndex >= array.length)){
+			status = false;
+		}
+		return status;
 	}
 }
