86f20f5b21027cd46e1cb3be2d544ce0da98e739
Pedro Nascimento da Silva <101910729+Pedronrb@users.noreply.github.com>
2024-03-26 20:57:16 -0300

    Update QueueImpl.java

--- R07-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueImpl.java
+++ R07-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueImpl.java
@@ -2,61 +2,61 @@ package adt.queue;
 
 public class QueueImpl<T> implements Queue<T> {
 
-    private T[] array;
-    private int tail;
+	private T[] array;
+	private int tail;
 
-    @SuppressWarnings("unchecked")
-    public QueueImpl(int size) {
-        array = (T[]) new Object[size];
-        tail = -1;
-    }
+	@SuppressWarnings("unchecked")
+	public QueueImpl(int size) {
+		this.array = (T[]) new Object[size];
+		this.tail = -1;
+	}
 
-    @Override
-    public T head() {
-        if (isEmpty()) {
-            try {
-				throw new QueueUnderflowException();
-			} catch (QueueUnderflowException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-        return array[0];
-    }
+	@Override
+	public T head() {
+		T head = null;
+		if (!isEmpty()) {
+			head = this.array[0];
+			;
+		}
+		return head;
+	}
 
-    @Override
-    public boolean isEmpty() {
-        return tail == -1;
-    }
+	@Override
+	public boolean isEmpty() {
+		return this.tail == -1;
+	}
 
-    @Override
-    public boolean isFull() {
-        return tail == array.length - 1;
-    }
+	@Override
+	public boolean isFull() {
+		return this.tail == this.array.length - 1;
+	}
 
-    private void shiftLeft() {
-        for (int i = 0; i < tail; i++) {
-            array[i] = array[i + 1];
-        }
-        tail--;
-    }
+	private void shiftLeft() {
+		for (int i = 0; i < this.tail; i++) {
+			this.array[i] = this.array[i + 1];
+		}
+	}
 
-    @Override
-    public void enqueue(T element) throws QueueOverflowException {
-        if (isFull()) {
-            throw new QueueOverflowException();
-        }
-        tail++;
-        array[tail] = element;
-    }
+	@Override
+	public void enqueue(T element) throws QueueOverflowException {
+		if (isFull()) {
+			throw new QueueOverflowException();
+		}
+		if (element != null) {
+			this.array[++this.tail] = element;
+		}
 
-    @Override
-    public T dequeue() throws QueueUnderflowException {
-        if (isEmpty()) {
-            throw new QueueUnderflowException();
-        }
-        T element = array[0];
-        shiftLeft();
-        return element;
-    }
+	}
+
+	@Override
+	public T dequeue() throws QueueUnderflowException {
+		if (isEmpty()) {
+			throw new QueueUnderflowException();
+		}
+		T dequeued = this.array[0];
+		this.shiftLeft();
+		this.tail--;
+		return dequeued;
+	}
+
 }
